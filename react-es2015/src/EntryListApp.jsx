c4ac0fb7649eda2e261bcbe7d06f44750e2ae67a
Brandon Richey <brichey@teladoc.com>
2016-02-11 10:33:06 -0500

    Fixed the ES2015 example

--- react-es2015/src/EntryListApp.jsx
+++ react-es2015/src/EntryListApp.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class App extends React.Component({
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -9,29 +9,34 @@ class App extends React.Component({
       searchFor: "",
       wip: ""
     };
-  },
+    this.removeEntry      = this.removeEntry.bind(this);
+    this.addEntry         = this.addEntry.bind(this);
+    this.updateWIP        = this.updateWIP.bind(this);
+    this.updateSearchTerm = this.updateSearchTerm.bind(this);
+    this.searchEntries    = this.searchEntries.bind(this);
+  }
   removeEntry(entry) {
     var newEntries = this.state.entries.filter(e => {
       return e !== entry;
     });
     this.setState({ entries: newEntries });
-  },
+  }
   addEntry(entry) {
     this.state.entries.push(entry);
     this.setState({ entries: this.state.entries, wip: "" });
-  },
+  }
   updateWIP(entry) {
     this.setState({ wip: entry });
-  },
+  }
   updateSearchTerm(entry) {
     this.setState({ searchFor: entry });
-  },
+  }
   searchEntries() {
     var searchFor = this.state.searchFor;
     return this.state.entries.filter(entry => {
       return (entry.indexOf(searchFor) > -1);
     });
-  },
+  }
   render() {
     return (
       <div>
@@ -45,23 +50,31 @@ class App extends React.Component({
       </div>
     );
   }
-});
+};
 
-class SearchForm extends React.Component({
-    search() {
-      this.props.updateSearchTerm(this.refs.search.value);
-    },
-    render() {
-      return (
-        <input type="text" placeholder="Search" ref="search" onInput={this.search}/>
-      );
-    }
-});
+class SearchForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.search = this.search.bind(this);
+  }
+  search() {
+    this.props.updateSearchTerm(this.refs.search.value);
+  }
+  render() {
+    return (
+      <input type="text" placeholder="Search" ref="search" onInput={this.search}/>
+    );
+  }
+};
 
-class EntryList extends React.Component({
+class EntryList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.renderEntry = this.renderEntry.bind(this);
+  }
   renderEntry(entry) {
     return (<EntryItem entry={entry} key={entry} removeEntry={this.props.removeEntry}/>);
-  },
+  }
   render() {
     return (
       <ul>
@@ -69,12 +82,16 @@ class EntryList extends React.Component({
       </ul>
     );
   }
-});
+};
 
-class EntryItem extends React.Component({
+class EntryItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.remove = this.remove.bind(this);
+  }
   remove() {
     this.props.removeEntry(this.props.entry);
-  },
+  }
   render() {
     return (
       <li key={this.props.entry}>
@@ -83,25 +100,36 @@ class EntryItem extends React.Component({
       </li>
     );
   }
-});
+};
 
-class EntryForm extends React.Component({
+class EntryForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.updateEntry   = this.updateEntry.bind(this);
+    this.addEntry      = this.addEntry.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
   addEntry() {
     this.props.addEntry(this.refs.entry.value);
     this.refs.entry.value = "";
-  },
+  }
   updateEntry() {
     this.props.updateWIP(this.refs.entry.value);
-  },
+  }
+  handleKeyDown(event) {
+    if (event.keyCode === 13) {
+      this.addEntry();
+    }
+  }
   render() {
     return (
       <div>
-        <input type="text" ref="entry" onInput={this.updateEntry}/>
+        <input type="text" ref="entry" onInput={this.updateEntry} onKeyDown={this.handleKeyDown}/>
         <button onClick={this.addEntry}>Add</button>
       </div>
     );
   }
-});
+};
 
 ReactDOM.render(
   <App/>,
